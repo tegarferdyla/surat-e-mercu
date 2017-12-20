3c61a06f63aa762749f86b54274a35173e2cc5c2
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2017-12-20 19:20:02 +0700

    add controller mahasiswa

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mahasiswa extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Mahasiswa.php */
+/* Location: ./application/controllers/Mahasiswa.php */
