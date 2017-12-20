77818d26a9179e7bcaeb196b0b81b8606c9b8043
dalas98 <dalas98@gmail.com>
2017-12-20 21:56:44 +0700

    edit
    fix kekurangan >

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -29,7 +29,7 @@
                   <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
                 </div>
                 <!-- /.col -->
-              </div
+              </div>
           </div>
           <!-- /.login-box-body -->
         </div>
