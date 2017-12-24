705f5b67b05e9251e99defb488ccd885a1beff29
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2017-12-24 08:41:44 +0700

    formkp

--- application/views/mahasiswa/header.php
+++ application/views/mahasiswa/header.php
@@ -12,7 +12,7 @@
 
 </head>
 <body>
-	<nav class="navbar navbar-default" data-spy="affix" data-offset-top="197">
+	<nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -22,18 +22,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo site_url('home') ?>">Brand</a>
+      <a class="navbar-brand" href="<?php echo site_url('home') ?>"><img src="<?php echo base_url('assets/image/UMB.png')?>" alt="Home" width="75"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Daftar</a></li>
-       <li><a href="<?php echo site_url('login') ?>">Login</a></li>
+       <li><a href="#">Lihat Status</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-<h1>selamaaaaat</h1>
-<h1>selamaaaaat</h1>
-
