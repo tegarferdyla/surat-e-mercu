915f5aaf873b4d2ab05adeb7013154d44a1af2a2
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2017-12-26 08:28:18 +0700

    content and footer modify

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -1,5 +1,32 @@
 <div class="container-fluid header-home">
+	
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+		<img src="<?php echo base_url('assets/image/umb.png') ?>" class="img-responsive center-block" alt="">
+	</div>
+	</div>
+
+	<div class="row">
+		<h1 class="text-center">E-SURAT FAKULTAS ILMU KOMPUTER</h1>
+	</div>
+	<div class="row">
+		<div class=" col-xs-4 col-sm-4 col-md-3 col-md-offset-4 col-sm-offset-3 col-xs-offset-2 ">
+			<a href="" class="btn btn-primary btn-header">DAFTAR</a>
+		</div>
+			<div class="col-md-4 col-sm-3 col-xs-2">
+				<a href="<?php echo site_url('login') ?>" class="btn btn-danger btn-header">LOGIN</a>
+			</div>
+	</div>
+</div>
+
+
+
+<div class="container-fluid">
 	<div class="container">
-		<h1 class="text-center">SELAMAT DATANG</h1>
+		<div class="row">
+			<h1 class="text-center">SELAMAT DATANG DI WEBSITE E-SURAT FAKULTAS ILMU KOMPUTER</h1>
+			<hr>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sed porro, aliquam hic! Quidem iusto, et impedit, nobis consequuntur dicta repellat perspiciatis, eius ipsam laborum ullam autem nostrum sint fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis cumque doloribus eveniet saepe error fuga dolore quidem ut, delectus. Dolores tempore nesciunt culpa amet architecto, ea itaque odit neque eligendi.</p>
+		</div>
 	</div>
 </div>
