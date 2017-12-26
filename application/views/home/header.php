3d2015362e3da1e2e7732ea79bd529f8276cb6a1
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2017-12-26 08:28:00 +0700

    header

--- application/views/home/header.php
+++ application/views/home/header.php
@@ -12,8 +12,8 @@
 
 </head>
 <body>
-	<nav class="navbar navbar-default" data-spy="affix" data-offset-top="197">
-  <div class="container-fluid">
+  <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -22,18 +22,19 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo site_url('home') ?>">Brand</a>
+     <a class="navbar-brand" href="<?php echo site_url('home')?>"><img src="<?php echo base_url('assets/image/UMB.png')?>" alt="Home" width="75"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Daftar</a></li>
-       <li><a href="<?php echo site_url('login') ?>">Login</a></li>
+        <li><a href="<?php echo site_url('home')?>">Beranda</a></li>
+       <li><a href="#">Tentang</a></li>
+       <li><a href="">Bantuan</a></li>
+       <li> <a href="">Kontak</a></li>
       </ul>
+    
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-<h1>selamaaaaat</h1>
-<h1>selamaaaaat</h1>
 
