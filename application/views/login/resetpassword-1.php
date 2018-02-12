81cee92185ddd5863a5c805a4035c037541d71bc
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-02-12 10:43:19 +0700

    add resetpassword

---
+++ application/views/login/resetpassword-1.php
@@ -0,0 +1,36 @@
+ <div class="container-fluid">
+     <div class="container">
+
+       
+       <div class="row">
+         <div class="col-md-4 col-md-offset-4 box-login">
+           <?php echo form_open('login/ceklogin'); ?>
+           <h3 class="text-center" style="color: white;  ">2</h3>      
+             <h3 class="text-center title-login">Masukkan Password Baru Anda</h3>
+              <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Password" name="username">
+              </div>
+                <div class="form-group has-feedback">
+                  <a href="<?php echo site_url('login/resetpassword1') ?>" class="btn btn-danger btn-block"><b>SUBMIT</b><i class="fas fa-arrow-circle-right pull-right fa-lg"></i></a>
+                </div>
+         </div>
+       </div>
+       <div class="row">
+                <!-- /.col -->
+                
+                <!-- /.col -->
+              </div>
+          </div>
+          <?php echo form_close(); ?>
+     </div>
+          
+              
+          <!-- /.login-box-body -->
+        
+
+
+
+<script src="<?php echo base_url('assets/plugins/jquery/dist/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/bootstrap/dist/js/bootstrap.min.js')?>" ></script>
+</body>
+</html>
