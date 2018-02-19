bc5126a13c9f2ec02932fbe9f96c4669e46ca1fa
agungpradanaw <33536926+agungpradanaw@users.noreply.github.com>
2018-02-19 21:47:43 +0700

    ChartJS
    Print-view (-)

--- application/views/tester/headerChart.php
+++ application/views/tester/headerChart.php
@@ -25,3 +25,8 @@
 
 </head>
 <body>
+  <header class="header">
+    <center><h3 style="color: #112EA5">LAPORAN PEMBUATAN SURAT DAN JUMLAH</h3></center>
+    <center><h3 style="color: #112EA5">AKUN E-SURAT</h3></center>
+    <center><h3 style="color: #E219B7">01/01/2018 ~ 01/02/2018</h3></center>
+  </header>
