38c5dc0cbe8cc930f3c9cd57b0075d508630437f
ivalrival <ivalrival95@gmail.com>
2018-02-20 21:02:46 +0700

    perapihan koding

--- application/views/mahasiswa/header.php
+++ application/views/mahasiswa/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title>E-Surat Mercu Buana</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <title>E-Surat Mercu Buana</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo base_url('assets/plugins/bootstrap/dist/css/bootstrap.min.css') ?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/plugins/font-awesome/css/font-awesome.min.css') ?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css') ?>">
@@ -11,8 +12,9 @@
   <link rel="stylesheet" href="<?php echo base_url('assets/plugins/datatables.net-bs/css/dataTables.bootstrap.min.css')?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/style.css') ?>">
 </head>
+
 <body>
-	<nav class="navbar navbar-default">
+  <nav class="navbar navbar-default">
     <div class="row" style="margin-left: 1%;">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
@@ -24,14 +26,19 @@
         </button>
         <a class="navbar-brand" href="<?php echo site_url('') ?>"><img src="<?php echo base_url('assets/image/UMB.png')?>" alt="Home" width="75"></a>
       </div>
-
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
-          <li class="status"><?php echo $this->session->userdata('nama_mahasiswa');?></li>
-          <li><a class="status" href="<?php echo site_url('mahasiswa/lihat') ?>">Lihat Status</a></li>
-          <li class="keluar"><?php echo anchor('login?logout=signout', 'keluar');?></li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+          <li class="status">
+            <?php echo $this->session->userdata('nama_mahasiswa');?></li>
+            <li><a class="status" href="<?php echo site_url('mahasiswa/lihat') ?>">Lihat Status</a>
+            </li>
+            <li class="keluar">
+              <?php echo anchor( 'login?logout=signout', 'keluar');?>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
